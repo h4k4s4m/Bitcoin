27f19f5c814483703a8fd65e86409d015553d6bb
Sahm Samarghandi <h4k4s4m@gmail.com>
2014-07-25 14:37:40 -0700

    working SQL cleanups

--- asql.py
+++ asql.py
@@ -2,4 +2,4 @@ import sqlalchemy
 
 def clean_sql_every_x_hours(x):
     engine=sqlalchemy.create_engine('mysql://sam_coin:abc123!@mysql.samarghandi.com/samarghandi_bitcoin')
-    engine.execute("DELETE Coins WHERE Time>= now() - INTERVAL %s HOUR" % x)
+    engine.execute("DELETE FROM Coins WHERE Time>= SYSDATE() - INTERVAL %s HOUR" % x)
